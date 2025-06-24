316f24a601d5a55f942dd556b7bb6c211f91d8fa
maxsysss <40639191+maxsysss@users.noreply.github.com>
2025-06-24 16:04:03 -0300

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,34 @@
+package account;
+class BankAccount{
+    private String accNumber;
+    private double balance;
+    private String custName;
+    private String email;
+    private String phoneNo;
+    
+    public void setAccNumber(String accNumber){
+        this.accNumber = accNumber;
+    }
+    public void setCustName(String name){
+        this.custName = name;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setPhoneNo(String phoneNo){
+        this.phoneNo = phoneNo;
+    }
+    public void deposit(double amount){
+        balance += amount;
+    }
+    public void withdraw(double amount){
+        if(this.balance - amount <= 0){
+            System.out.println("No suficient funds to withdraw");
+        }else{
+            System.out.println("You withdraw: $" + (balance - amount));
+        }
+    }
+    public String toString(){
+        return "Balance: " + balance + "\nAccount number: " + accNumber + "\nName: " + custName + "\nE-mail: " + email + "\nPhone: " +phoneNo;
+    }
+}
