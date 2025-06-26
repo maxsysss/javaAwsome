3d3962ce17bf9ccc77f0cab4255a177d910f8846
maxsysss <40639191+maxsysss@users.noreply.github.com>
2025-06-26 11:20:12 -0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,1 +1,28 @@
-
+public class Customer{
+    private String name;
+    private double cLimit;
+    private String email;
+    
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public double getLimit(){
+        return cLimit;
+    }
+    
+    public Customer(String name, double cLimit, String email){
+        this.name = name;
+        this.cLimit = cLimit;
+        this.email = email;
+    }
+    public Customer(){
+        this("AAAAA","AAAAA@BBB.CC");
+    }
+    public Customer(String name, String email){
+        this.cLimit = 2000.0d
+        this(name, cLimit, email);
+    }
+}
