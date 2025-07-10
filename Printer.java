0e2491b74f804a93c990a1fb2ef7ecbd2607be1e
maxsysss <40639191+maxsysss@users.noreply.github.com>
2025-07-10 10:42:14 -0300

    Update Printer.java

--- Printer.java
+++ Printer.java
@@ -1,1 +1,44 @@
+class Printer{
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+    
+    public Printer(int t, int p, boolean d){
+        tonerLevel = t;
+        pagesPrinted = p;
+        duplex = d
+    }
+    
+    public void getTonerLevel(){
+        System.out.println("Toner Level: " + tonerLevel + "%");
+    }
+    public void getPagesPrinted(){
+        System.out.println("Pages printed: "+pagesPrinted+" pages");
+    }
+    
+    public int addToner(int tonerAmount){
+        tonerLevel+= tonerAmount;
+        if(tonerLevel>100){
+            tonerLevel=100;
+        }else if(tonerLevel<0){
+            tonerLevel=0;
+        }
+        return tonerLevel;
+    }
+    public int printPages(int pages){
+        if (duplex){
+            pagesPrinted+=pages/2;
+        }else{
+            pagesPrinted+=pages;
+        }
+        return pagesPrinted
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+
+	}
+}
 
