aadc22831547153d510cc10004b202997b770ca2
maxsysss <40639191+maxsysss@users.noreply.github.com>
2025-07-14 11:45:27 -0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,15 +1,23 @@
+
+
+
+/*
 class HybridCar extends Car{
     private double avgKmPerLitre;
     private int batterySize;
-    private in cylinders;
+    private int cylinders;
+    
+    public HybridCar(String d){
+        super(d);
+    }
     
     @Override
-    public void startEngine(this){
+    public void startEngine(Object this){
         super(this);
         //System.out.println("HybridCar start engine!")
     }
     @Override
-    protected void drive(this){
+    protected void drive(Object this){
         super(this);
         //System.out.println("HybridCar engine is running");
     }
@@ -23,13 +31,16 @@ class EletricCar extends Car{
     private double avgKmPerCharge;
     private int batterySize;
     
-    @Override
-    public void startEngine(this){
-        super(this);
-        System.out.println("EletricCar start engine!")
+    public EletricCar(String d){
+        super(d);
     }
     @Override
-    protected void drive(this){
+    public void startEngine( EletricCar this){
+        super(this);
+        System.out.println("EletricCar start engine!");
+    }
+    @Override
+    protected void drive(EletricCar this){
         super(this);
         //System.out.println("EletricCar engine is running");
     }
@@ -42,13 +53,16 @@ class GasPoweredCar extends Car{
     private double avgKmPerLitre;
     private int cylinders;
     
+    public GasPoweredCar(String d){
+        super(d);
+    }
     @Override
-    public void startEngine(this){
+    public void startEngine(GasPoweredCar this){
         super(this);
         //System.out.println("GasPoweredCar start engine!")
     }
     @Override
-    protected void drive(this){
+    protected void drive(GasPoweredCar this){
         super(this);
         //System.out.println("GasPoweredCar engine is running");
     }
@@ -60,6 +74,10 @@ class GasPoweredCar extends Car{
 
 public class Car{
     String description;
+    
+    public Car(String d){
+        description=d;
+    }
     
     public void startEngine(Car c){
         System.out.println(c + ": start engine!");
@@ -77,9 +95,12 @@ public class Car{
     
     
     public static void main(String[] args){
-        
+        Car c = new Car("AAAA");
+        c.runEngine(this);
     }
 }
+
+*/
 
 
 /*
