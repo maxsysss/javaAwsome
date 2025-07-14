e1091030fcc834ac5ef3bcf7a21650484bac0b40
maxsysss <40639191+maxsysss@users.noreply.github.com>
2025-07-14 12:16:13 -0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,4 +1,138 @@
+/*
 
+
+
+class HybridCar extends Car{
+    private double avgKmPerLitre;
+    private int batterySize;
+    private int cylinders;
+    
+    public HybridCar(String d){
+        super(d);
+    }
+    
+    @Override
+    public void startEngine(Object this){
+        super(this);
+        //System.out.println("HybridCar start engine!")
+    }
+    @Override
+    protected void drive(Object this){
+        super(this);
+        //System.out.println("HybridCar engine is running");
+    }
+    public String toString(){
+        return "Hybrid Car";
+    }
+}
+
+
+class EletricCar extends Car{
+    private double avgKmPerCharge;
+    private int batterySize;
+    
+    public EletricCar(String d){
+        super(d);
+    }
+    @Override
+    public void startEngine( EletricCar this){
+        super(this);
+        System.out.println("EletricCar start engine!");
+    }
+    @Override
+    protected void drive(EletricCar this){
+        super(this);
+        //System.out.println("EletricCar engine is running");
+    }
+    public String toString(){
+        return "Eletric Car";
+    }
+}
+
+class GasPoweredCar extends Car{
+    private double avgKmPerLitre;
+    private int cylinders = 6;
+    
+    public GasPoweredCar(String d){
+        super(d);
+    }
+    public GasPoweredCar(String d,double kmPL, int cyl){
+        super(d);
+        avgKmPerLitre=kmPL;
+        cylinders=cyl;
+    }
+    
+    @Override
+    public void startEngine(){
+        super.startEngine();
+        System.out.printf("Gas -> All %d cylinders are fired up, Ready!%n", cylinders);
+    }
+    @Override
+    protected void runEngine(){
+        System.out.printf("Gas -> usage exceeds the average: %.2f %n", avgKmPerLitre);
+    }
+    /*
+    @Override
+    protected void drive(GasPoweredCar this){
+        super(this);
+        //System.out.println("GasPoweredCar engine is running");
+    }
+    */
+    /*
+    public String toString(){
+        return "GasPowered Car";
+    }
+    
+    
+}
+
+
+class Car{
+    private String description;
+    
+    public Car(String d){
+        description=d;
+    }
+    
+    public void startEngine(){
+        System.out.println("Car -> start engine!");
+    }
+    protected void runEngine(){
+        System.out.println("Car -> starts to move!");
+    }
+    public void drive(){
+        System.out.println("Car -> driving type is: " + getClass().getSimpleName());
+        runEngine();
+    }
+    
+    public String toString(){
+        return "Car";
+    }
+}
+
+public class NextMain2{
+    
+    public static void main(String[] args){
+        Car car = new Car("2022 Blue Ferrari 296 GTS");
+        runRace(car);
+        Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS", 15.4, 6);
+        runRace(ferrari);
+    }
+    
+    public static void runRace(Car car){
+        car.startEngine();
+        car.drive();
+    }
+    
+}
+    
+
+
+
+
+
+
+*/
 
 
 /*
