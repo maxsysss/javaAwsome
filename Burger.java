45111375cd7e5a9cf47edfbbe981fb18c0cbf411
maxsysss <40639191+maxsysss@users.noreply.github.com>
2025-07-15 15:52:03 -0300

    Update Burger.java

--- Burger.java
+++ Burger.java
@@ -1,1 +1,49 @@
+//Item.java
 
+public class Item{
+    private String type;
+    private String name;
+    private double price;
+    private String size="MEDIUM";
+    
+    public Item(String type, String name, double price){
+        this.type=type.toUpperCase();
+        this.name=name.toUpperCase();
+        this.price=price;
+    }
+    public String getName(){
+        if(type.equals("SIDE") || type.equals("DRINK")){
+            return size + " " + name;
+        }
+        return name;
+    }
+    public double getBasePrice(){
+        return price;
+    }
+    
+    public double getAdjustedPrice(){
+        return switch (size){
+            case "SMALL" -> getBasePrice() - 0.5;
+            case "LARGE" -> getBasePrice() + 1;
+            default -> getBasePrice();
+        };
+    }
+    public void setSize(String size){
+        this.size=size;
+    }
+    
+    public static void printItem(String name, double price){
+        System.out.printf("%20s:%6.2f%n", name, price);
+    }
+    public void printItem(){
+        printItem(getName(), getAdjustedPrice());
+    }
+    
+    
+    public static void main(String[] args){
+        Item coke = new Item("drink", "coke", 1.50);
+        coke.printItem();
+        coke.setSize("LARGE");
+        coke.printItem();
+    }
+}
